f94d592a9b7b0e9cede83c4a7ba11307d455ec22
Om Mehta <ommehta16@icloud.com>
2025-04-01 12:09:08 -0400

    terrible script for "live" rebranding

--- fool.js
+++ fool.js
@@ -1,10 +1,31 @@
-const a = () => {
+const infect = () => {
+    console.log(document.body.innerHTML);
+
+    let txt = String(document.body.innerHTML);
+    if (txt.includes("Princeton")) {
+        let i = txt.indexOf("Princeton");
+        document.body.innerHTML = txt.substring(0, i) + "WWP" + txt.substring(i + 9);
+    }
+    else if (txt.includes("PHS")) {
+        let i = txt.indexOf("PHS");
+        document.body.innerHTML = txt.substring(0, i) + "HSS" + txt.substring(i + 3);
+    }
+    else if (txt.includes("CPT")) {
+        let i = txt.indexOf("CPT");
+        document.body.innerHTML = txt.substring(0, i) + "WWPPC" + txt.substring(i + 3);
+    }
+    else return;
+
+    setTimeout(infect, 1000);
+}
+
+const fool = () => {
     const now = new Date();
     if (now.getMonth() + 1 != 4 || now.getDate() != 1) return;
 
     const dark = {
         "--background-body": "#000000",
-        "--background": "0f0f0f",
+        "--backgoround": "0f0f0f",
         "--background-alt": "#f7f7f7",
         "--selection": "#303030",
         "--text-main": "#d0d0d0",
@@ -14,11 +35,8 @@ const a = () => {
         "--form-text": "#e0e0e0",
     }
 
-    for (const style in dark) {
-        document.documentElement.style.setProperty(style, dark[style]);
-    }
+    for (const style in dark) document.documentElement.style.setProperty(style, dark[style]);
 
-    document.body.style.setProperty("color", "white");
+    setTimeout(infect, 100);
 }
-a();
-
+fool();
